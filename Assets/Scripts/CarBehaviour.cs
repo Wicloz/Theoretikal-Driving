d80a62c382c50366e62675a4abdacb31a56dcc76
Wilco de Boer <wfh@wilcodeboer.me>
2017-01-24 14:05:23 +0100

    Do things with roads

---
+++ Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarBehaviour : MonoBehaviour {
+    private CarController carScript;
+    public List<GameObject> path = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        carScript = this.GetComponent<CarController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RoadTile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public enum direction {forward, back, left, right};
+
+[Serializable]
+public struct orientation {
+    public direction entance;
+    public Vector3 rotation;
+    public Vector3 offset;
+}
+
+public class RoadTile : MonoBehaviour {
+    public List<orientation> orientations = new List<orientation>();
+    public List<RoadTilePath> paths = new List<RoadTilePath>();
+    public List<direction> shape = new List<direction>();
+
+    private int maxspeed;
+    private Dictionary<direction, RoadTile> links = new Dictionary<direction, RoadTile>();
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
